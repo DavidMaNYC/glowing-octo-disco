8ba2efd0d4b41c241ed5c2eb55b531b0b9830105
DavidMaNYC <DavidmaBK@gmail.com>
2024-06-21 17:43:43 -0400

    first commit

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,43 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  let entries = [];
+  let completedSources = 0;
+
+  logSources.forEach((source, index) => {
+    const entry = source.pop();
+    if (entry) {
+      entries[index] = entry;
+    } else {
+      completedSources++;
+    }
+  });
+
+  while (completedSources < logSources.length) {
+    let smallestIndex = -1;
+    let smallestEntry = null;
+    for (let i = 0; i < entries.length; i++) {
+      if (
+        entries[i] &&
+        (smallestEntry === null || entries[i].date < smallestEntry.date)
+      ) {
+        smallestEntry = entries[i];
+        smallestIndex = i;
+      }
+    }
+
+    if (smallestIndex !== -1) {
+      printer.print(smallestEntry);
+
+      const nextEntry = logSources[smallestIndex].pop();
+      if (nextEntry) {
+        entries[smallestIndex] = nextEntry;
+      } else {
+        entries[smallestIndex] = null;
+        completedSources++;
+      }
+    }
+  }
+
+  printer.done();
 };
